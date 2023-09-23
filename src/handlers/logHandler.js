c61a5c1c5c2ef0a34e255d05711133342c7a96ac
Jaka Kosten <kosten.jaka93@gmail.com>
2023-09-23 13:17:38 +0200

    Added a folder with a logHandler

---
+++ src/handlers/logHandler.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+
+const logsDirectory = "./logs/";
+if (!fs.existsSync(logsDirectory)) {
+  fs.mkdirSync(logsDirectory);
+}
+if (!fs.existsSync(`${logsDirectory}/all_logs.log`)) {
+  fs.writeFileSync(`${logsDirectory}/all_logs.log`, "");
+}
+if (!fs.existsSync(`${logsDirectory}/registration.log`)) {
+  fs.writeFileSync(`${logsDirectory}/registration.log`, "");
+}
+if (!fs.existsSync(`${logsDirectory}/update.log`)) {
+  fs.writeFileSync(`${logsDirectory}/update.log`, "");
+}
+
+const logsStream = fs.createWriteStream(`${logsDirectory}/all_logs.log`, {
+  flags: "a",
+});
+const registrationStream = fs.createWriteStream(
+  `${logsDirectory}/registration.log`,
+  {
+    flags: "a",
+  }
+);
+const updateLogs = fs.createWriteStream(`${logsDirectory}/update.log`, {
+  flags: "a",
+});
+
+module.exports = { logsStream, registrationStream, updateLogs };
