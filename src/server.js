fba4d65fd3d633d992d611328bd0956ff95f2e56
Jaka Kosten <kosten.jaka93@gmail.com>
2023-10-19 10:51:26 +0200

    First commit

--- src/server.js
+++ src/server.js
@@ -3,8 +3,6 @@ if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 
-console.log("Hello");
-
 const express = require("express");
 const app = express();
 const bcrypt = require("bcrypt");
